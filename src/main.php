6b19a84253b6467661106855d6b2a9eaa0a6fd02
Kharlamov Vladislav <kharlamov.workspace@gmail.com>
2025-08-02 15:21:49 +0300

    RedisLock API extension

--- src/main.php
+++ src/main.php
@@ -18,16 +18,17 @@ class RedisLock
     public function lock(string $resource, int $ttlMillis): ?string
     {
         $lock_uuid = uniqid(more_entropy: true);
-        $startTime = microtime(ture);
+        $startTime = microtime(true);
 
+        $isSet = false;
         try {
-            $this->redisInstance->set($resource, $lock_uuid, ["NX", "PX" => $ttlMillis]);
+            $isSet = $this->redisInstance->set($resource, $lock_uuid, ["NX", "PX" => $ttlMillis]);
         } catch (RedisException $e) {
             error_log("Unable to aquire lock. Cause: " . $e->getMessage() . PHP_EOL);
         }
 
         $elapsedTimeMillis = (microtime(true) - $startTime) * MILLISECOND;
-        if ($elapsedTimeMillis < $ttlMillis) {
+        if ($isSet === true && $elapsedTimeMillis < $ttlMillis) {
             return $lock_uuid;
         } else {
             $this->unlock($resource, $lock_uuid);
@@ -35,17 +36,34 @@ class RedisLock
         }
     }
 
-    public function unlock(string $resource, string $lock_uuid): void {
+    public function unlock(string $resource, string $lock_uuid): void
+    {
         try {
             $unlockScript = "if redis.call('get', KEYS[1]) == ARGV[1] then return redis.call('del', KEYS[1]) else return 0 end";
             $this->redisInstance->eval($unlockScript, [$resource, $lock_uuid], 1);
-        } catch(RedisException $e) {
+        } catch (RedisException $e) {
             error_log("Unable to release lock. Cause: " . $e->getMessage() . PHP_EOL);
+        }
+    }
+
+    public function runBlocking(string $resource, callable $criticalSection): void
+    {
+        $shouldRun = true;
+        while ($shouldRun) {
+            if ($lock_id = $this->lock("main", 1200 * 1000)) {
+                
+                $criticalSection();
+
+                $this->unlock("main", $lock_id);
+                $shouldRun = false;
+            } else {
+                usleep(10 * MILLISECOND);
+            }
         }
     }
 }
 
-$
+echo "Started";
 
 $redlock = new RedisLock([
     "host" => "localhost",
@@ -53,13 +71,12 @@ $redlock = new RedisLock([
     "timeout" => "0.5",
 ]);
 
-$shouldRun = true;
-while ($shouldRun) {
-    if ($lock_id = $redlock->lock("main", 5000)) {
-        sleep(1);
-        echo "Hello";
-        $redlock->unlock("main", $lock_id);
-    } else {
-        usleep(10 * MILLISECOND);
-    }
-}
+$optinos = getopt("", ["value:"]);
+$value = intval($optinos["value"]) ?? null;
+if ($value === null)
+    exit(1);
+
+$redlock->runBlocking("main", function() use ($value): void {
+    sleep(2);
+    echo "Hello #$value";
+});
