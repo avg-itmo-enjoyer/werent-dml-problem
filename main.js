5449bf2a1b86efde9d4a4f3edac17cfed0005ab1
Kharlamov Vladislav <kharlamov.workspace@gmail.com>
2025-08-06 01:18:07 +0300

    Refactor code + add README.md

--- main.js
+++ main.js
@@ -2,9 +2,15 @@ import { spawn } from "node:child_process";
 
 const main = () => {
     const n = 10;
+    const minTime = 1; // min sleep time is 1 sec
+    const maxTime = 5; // max sleep time is 5 sec
 
     [...Array(n).keys()].forEach(idx => {
-        const job = spawn("php", ["./src/main.php", `--value=${idx + 1}`]);
+        const job = spawn("php", [
+            "./src/main.php", 
+            `--value=${idx + 1}`,
+            `--sleep-time=${Math.random() * (maxTime - minTime + 1) + minTime}`
+        ]);
 
         // TODO (kharlamov_vs): propper configurable looging
         job.stdout.on("data", (data) => console.log(`${data}`))
--- src/main.php
+++ src/main.php
@@ -1,82 +1,25 @@
 <?php
+require_once dirname(__DIR__) . "/vendor/autoload.php";
+
+use App\Lock\RedisLock;
+
+define("SECOND", 1000);
 define("MILLISECOND", 1000);
 
-class RedisLock
-{
-    private Redis $redisInstance;
+$redlock = RedisLock::default();
 
-    public function __construct(array $redisConfig)
-    {
-        $this->redisInstance = new Redis();
-        $this->redisInstance->connect(
-            $redisConfig["host"],
-            $redisConfig["port"],
-            $redisConfig["timeout"],
-        );
-    }
-
-    public function lock(string $resource, int $ttlMillis): ?string
-    {
-        $lock_uuid = uniqid(more_entropy: true);
-        $startTime = microtime(true);
-
-        $isSet = false;
-        try {
-            $isSet = $this->redisInstance->set($resource, $lock_uuid, ["NX", "PX" => $ttlMillis]);
-        } catch (RedisException $e) {
-            error_log("Unable to aquire lock. Cause: " . $e->getMessage() . PHP_EOL);
-        }
-
-        $elapsedTimeMillis = (microtime(true) - $startTime) * MILLISECOND;
-        if ($isSet === true && $elapsedTimeMillis < $ttlMillis) {
-            return $lock_uuid;
-        } else {
-            $this->unlock($resource, $lock_uuid);
-            return null;
-        }
-    }
-
-    public function unlock(string $resource, string $lock_uuid): void
-    {
-        try {
-            $unlockScript = "if redis.call('get', KEYS[1]) == ARGV[1] then return redis.call('del', KEYS[1]) else return 0 end";
-            $this->redisInstance->eval($unlockScript, [$resource, $lock_uuid], 1);
-        } catch (RedisException $e) {
-            error_log("Unable to release lock. Cause: " . $e->getMessage() . PHP_EOL);
-        }
-    }
-
-    public function runBlocking(string $resource, callable $criticalSection): void
-    {
-        $shouldRun = true;
-        while ($shouldRun) {
-            if ($lock_id = $this->lock("main", 1200 * 1000)) {
-                
-                $criticalSection();
-
-                $this->unlock("main", $lock_id);
-                $shouldRun = false;
-            } else {
-                usleep(10 * MILLISECOND);
-            }
-        }
-    }
-}
-
-echo "Started";
-
-$redlock = new RedisLock([
-    "host" => "localhost",
-    "port" => "6379",
-    "timeout" => "0.5",
-]);
-
-$optinos = getopt("", ["value:"]);
-$value = intval($optinos["value"]) ?? null;
-if ($value === null)
+$optinos = getopt("", ["value:", "sleep-time:"]);
+$value = $optinos["value"] ?? null;
+$sleepTime = $optinos["sleep-time"] ?? null;
+if ($value === null || $sleepTime === null)
     exit(1);
 
-$redlock->runBlocking("main", function() use ($value): void {
-    sleep(2);
-    echo "Hello #$value";
+$value = intval($value);
+$sleepTime = intval($sleepTime);
+echo "Started #$value" . PHP_EOL;
+$redlock->runBlocking("main", function() use ($value, $sleepTime): void {
+    $startTime = microtime(true);
+    sleep($sleepTime);
+    $executionTime = number_format(microtime(true) - $startTime, 3) * MILLISECOND;
+    echo "Hello #$value. Execution time: ${executionTime}ms" . PHP_EOL;
 });
